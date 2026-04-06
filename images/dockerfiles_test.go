6e34538747d3ec8317cc28e0a6214657672b4c4c
Lachlan Donald <lachlan@buildkite.com>
2026-04-06 17:36:57 +1000

    feat: add ssh client tools to base images

--- images/dockerfiles_test.go
+++ images/dockerfiles_test.go
@@ -30,6 +30,29 @@ func TestPublishedBaseImagesInstallBash(t *testing.T) {
 	}
 }
 
+func TestPublishedBaseImagesInstallOpenSSHClientDefault(t *testing.T) {
+	t.Parallel()
+
+	for _, relPath := range []string{
+		"Dockerfile.base-image",
+		"Dockerfile.base-image-docker",
+		"Dockerfile.base-image-agents",
+	} {
+		relPath := relPath
+		t.Run(relPath, func(t *testing.T) {
+			t.Parallel()
+
+			raw, err := os.ReadFile(filepath.Join(".", relPath))
+			if err != nil {
+				t.Fatalf("read %s: %v", relPath, err)
+			}
+			if !strings.Contains(string(raw), "\n  openssh-client-default \\\n") {
+				t.Fatalf("%s does not install openssh-client-default", relPath)
+			}
+		})
+	}
+}
+
 func TestPublishedBaseImagesExposeMiseShimsOnPATH(t *testing.T) {
 	t.Parallel()
 
